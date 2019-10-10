6255d571a4ef671b71d7c81c1853154b40be4f36
Kanderao Laxmi Narasimha Rao <klnsuman@gmail.com>
2019-10-10 12:03:21 +0530

    Add files via upload

---
+++ Python/Queue/Queue.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 18:05:34 2019
+
+@author: klrao
+Queue Implemengtation
+"""
+import sys 
+
+class ArrayQueue:
+
+   DEFAULT_CAPACITY = 10
+   
+   def __init__(self):
+       self._data = [None]*ArrayQueue.DEFAULT_CAPACITY
+       self._size = 0
+       self._front = 0
+       
+   def __len__(self):
+       return len(self._data)
+   
+    
+   def isEmpty(self):
+       return self._size == 0
+   
+   def enqueue(self,e):
+       if(self._size == len(self._data)):
+           self.resize(2*self._size)
+
+       avail = (self._front + self._size)%len(self._data)
+
+       self._data[avail] = e
+       self._size += 1
+       
+       
+   def dequeue(self):
+       if(self.isEmpty()):
+          raise sys.Empty("Queue is Empty")
+          
+       answer = self._data[self._front]   
+       self._front = (self._front+1)%len(self._data)
+    
+       self._size -= 1
+       return answer
+   
+   def first(self):
+       return self._data[self._front]
+   
+   
+   def resize(self,cap):
+       old = self._data
+       
+       self._data = [None]*cap
+       walk = self._front
+       
+       for k in range(cap):
+           self._data[k] = old[walk]
+           walk += 1
+           
+   def printQueue(self): 
+       walk = self._front
+       while (walk<self._size):
+           print(self._data[walk])
+           walk += 1
+def main():
+    Q = ArrayQueue() 
+    Q.enqueue(1)
+    Q.enqueue(2)
+    Q.dequeue()
+    Q.dequeue()
+    Q.dequeue()
+    Q.printQueue()
+
+if __name__ == '__main__':
+    main()       
